18f4c16b0b26c3d86f095835a93faef20bbc50cf
Lilium Wu <lilium.wu@yale.edu>
2017-06-12 12:00:16 -0700

    writes window.prompt version

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 /* Author: Lily Wu
   Date: June 12, 2017 */
+
+const $addNoteButton = document.querySelector('#addNoteButton');
+const $notesDiv = document.querySelector('#notesDiv');
+
+var $numNotesLabel = document.querySelector('#numNotesLabel');
+var numNotes = 0;
+
+// uses window.prompt rather than editable content
+function addNote() {
+  numNotes++;
+  console.log('numNotes is ' + numNotes);
+  $numNotesLabel.innerHTML = 'Current number of notes: ' + numNotes;
+  var result = window.prompt('Enter new note:', '');
+  var $newNote = document.createElement('p');
+  $newNote.innerHTML = '<span id="close" onclick="this.parentNode.parentNode.removeChild(this.parentNode); numNotes--; $numNotesLabel.innerHTML = &quot;Current number of notes: &quot; + numNotes; return false;">x</span> <br /><br />' + result;
+  $newNote.className = 'note';
+  $notesDiv.appendChild($newNote);
+}
+
+
+
+$addNoteButton.addEventListener('click', addNote);
