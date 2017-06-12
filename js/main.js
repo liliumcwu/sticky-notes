33ef9ee1e2ec759ba1a40fa899c2a3fd8c55c374
Lilium Wu <lilium.wu@yale.edu>
2017-06-12 12:19:53 -0700

    writes editable content version

--- js/main.js
+++ js/main.js
@@ -8,17 +8,39 @@ var $numNotesLabel = document.querySelector('#numNotesLabel');
 var numNotes = 0;
 
 // uses window.prompt rather than editable content
+// function addNote() {
+//   numNotes++;
+//   console.log('numNotes is ' + numNotes);
+//   $numNotesLabel.innerHTML = 'Current number of notes: ' + numNotes;
+//   var result = window.prompt('Enter new note:', '');
+//   var $newNote = document.createElement('p');
+//   $newNote.innerHTML = '<span id="close" onclick="this.parentNode.parentNode.removeChild(this.parentNode); numNotes--; $numNotesLabel.innerHTML = &quot;Current number of notes: &quot; + numNotes; return false;">x</span> <br /><br />' + result;
+//   $newNote.className = 'note';
+//   $notesDiv.appendChild($newNote);
+// }
+
+// uses editable content rather than window.prompt
 function addNote() {
   numNotes++;
-  console.log('numNotes is ' + numNotes);
-  $numNotesLabel.innerHTML = 'Current number of notes: ' + numNotes;
-  var result = window.prompt('Enter new note:', '');
-  var $newNote = document.createElement('p');
-  $newNote.innerHTML = '<span id="close" onclick="this.parentNode.parentNode.removeChild(this.parentNode); numNotes--; $numNotesLabel.innerHTML = &quot;Current number of notes: &quot; + numNotes; return false;">x</span> <br /><br />' + result;
+  $numNotesLabel.innerHTML = 'Current number of notes ' + numNotes;
+  var $newX = document.createElement('p');
+  $newX.innerHTML = 'x';
+  $newX.onclick = deleteNote;
+  var $noteText = document.createElement('p');
+  $noteText.contentEditable = true;
+
+  var $newNote = document.createElement('p')
   $newNote.className = 'note';
+  $newNote.appendChild($newX);
+  $newNote.appendChild($noteText);
   $notesDiv.appendChild($newNote);
 }
 
-
+function deleteNote() {
+  this.parentNode.parentNode.removeChild(this.parentNode);
+  numNotes--;
+  $numNotesLabel.innerHTML = 'Current number of notes: ' + numNotes;
+  return false;
+}
 
 $addNoteButton.addEventListener('click', addNote);
